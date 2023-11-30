b412ca97ffc8557fc355dc2fc39dcb4f6b9144e0
Dioxide <604834022@qq.com>
2023-11-30 16:36:54 +0800

    Project 1 Gold Complete

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,23 +8,41 @@ public class TestArrayDequeGold {
     public void testDeque() {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
-
+        int holder = 0;
+        StringBuilder msg = new StringBuilder();
         for (int i = 0; i < 1000; i++) {
-            Integer numberBetweenZeroAndHundred = StdRandom.uniform(1000);
-            Integer numberBetweenZeroAndHundred1 = StdRandom.uniform(1000);
-            sad.addLast(numberBetweenZeroAndHundred);
-            ads.addLast(numberBetweenZeroAndHundred);
-            sad.addFirst(numberBetweenZeroAndHundred1);
-            ads.addFirst(numberBetweenZeroAndHundred1);
-            Integer resultLastSad = sad.removeLast();
-            Integer resultLastAds = ads.removeLast();
-            Integer resultFirstSad = sad.removeFirst();
-            Integer resultFirstAds = ads.removeFirst();
-            assertEquals(numberBetweenZeroAndHundred, resultLastSad);
-            assertEquals(numberBetweenZeroAndHundred, resultLastAds);
-            assertEquals(numberBetweenZeroAndHundred1, resultFirstSad);
-            assertEquals(numberBetweenZeroAndHundred1, resultFirstAds);
-
+            double prob = StdRandom.uniform();
+            if (prob < 0.25) {
+                sad.addFirst(i);
+                ads.addFirst(i);
+                holder++;
+                msg.append("addFirst ").append("(").append(i).append(")").append(" \n");
+                assertEquals(msg.toString(), ads.get(0), sad.get(0));
+            } else if (prob < 0.5) {
+                sad.addLast(i);
+                ads.addLast(i);
+                holder++;
+                msg.append("addLast ").append("(").append(i).append(")").append(" \n");
+                assertEquals(msg.toString(), ads.get(holder - 1), sad.get(holder - 1));
+            } else if (prob < 0.75) {
+                if (ads.isEmpty()) {
+                    msg.append("isEmpty()\n");
+                    assertTrue(msg.toString(), sad.isEmpty());
+                    continue;
+                }
+                holder--;
+                msg.append("removeFirst ").append("(").append(")").append(" \n");
+                assertEquals(msg.toString(), ads.removeFirst(), sad.removeFirst());
+            } else {
+                if (ads.isEmpty()) {
+                    msg.append("isEmpty()\n");
+                    assertTrue(msg.toString(), sad.isEmpty());
+                    continue;
+                }
+                holder--;
+                msg.append("removeLast ").append("(").append(")").append(" \n");
+                assertEquals(msg.toString(), ads.removeLast(), sad.removeLast());
+            }
         }
     }
 }
