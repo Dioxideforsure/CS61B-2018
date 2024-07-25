fa42b50999ef5234454ed0405f3ad01dbbf40aa8
Dioxide <604834022@qq.com>
2024-07-25 15:40:47 +0800

    A big mistake between the definition of row and column, now fixed.

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -4,8 +4,15 @@ import java.util.*;
 
 public class SeamCarver {
     private Picture pic;
+    private int width;
+    private int height;
+    private boolean isVertical;
+
     public SeamCarver(Picture picture) {
         pic = picture;
+        width = picture.width();
+        height = picture.height();
+        isVertical = true;
     }
 
     public Picture picture() {
@@ -13,113 +20,195 @@ public class SeamCarver {
     }                       // current picture
 
     public int width() {
-        return pic.width();
+        return width;
     }                         // width of current picture
 
     public int height() {
-        return pic.height();
+        return height;
     }                        // height of current picture
 
     public double energy(int x, int y) {
-        int x1 = x - 1;
-        int x2 = x + 1;
-        int y1 = y - 1;
-        int y2 = y + 1;
-        if (x1 < 0) {
-            x1 = width() - 1;
-        }
-        if (x2 > width() - 1) {
-            x2 = 0;
-        }
-        if (y1 < 0) {
-            y1 = height() - 1;
-        }
-        if (y2 > height() - 1) {
-            y2 = 0;
-        }
-        double energyX = Math.pow((picture().get(x1, y).getRed() - picture().get(x2, y).getRed()), 2) +
-                Math.pow((picture().get(x1, y).getBlue() - picture().get(x2, y).getBlue()), 2) +
-                Math.pow((picture().get(x1, y).getGreen() - picture().get(x2, y).getGreen()), 2);
-        double energyY = Math.pow((picture().get(x, y1).getRed() - picture().get(x, y2).getRed()), 2) +
-                Math.pow((picture().get(x, y1).getBlue() - picture().get(x, y2).getBlue()), 2) +
-                Math.pow((picture().get(x, y1).getGreen() - picture().get(x, y2).getGreen()), 2);
-        return energyX + energyY;
-    }            // energy of pixel at column x and row y
+        int x1 = x + 1;
+        int x2 = x - 1; // x is column
+        int y1 = y + 1;
+        int y2 = y - 1; // y is row
 
+        if (x1 > width() - 1) {
+            x1 = 0;
+        }
+        if (x2 < 0) {
+            x2 = width() - 1;
+        }
+        if (y1 > height() - 1) {
+            y1 = 0;
+        }
+        if (y2 < 0) {
+            y2 = height() - 1;
+        }
+
+        int rx = pic.get(x, y1).getRed() - pic.get(x, y2).getRed();
+        int ry = pic.get(x1, y).getRed() - pic.get(x2, y).getRed();
+        int gx = pic.get(x, y1).getGreen() - pic.get(x, y2).getGreen();
+        int gy = pic.get(x1, y).getGreen() - pic.get(x2, y).getGreen();
+        int bx = pic.get(x, y1).getBlue() - pic.get(x, y2).getBlue();
+        int by = pic.get(x1, y).getBlue() - pic.get(x2, y).getBlue();
+
+        return Math.pow(rx, 2) + Math.pow(ry, 2) + Math.pow(gx, 2) + Math.pow(gy, 2) + Math.pow(bx, 2) + Math.pow(by, 2);
+    }                       // energy of pixel at column x and row y
 
 
     public int[] findHorizontalSeam() {
+        isVertical = false;
+        int[][][] minimumPointPosition = new int[height()][width()][2];
+        double[] multiMinEnergy = new double[height()];
+        for (int i = 0; i < height(); i++) {
+            multiMinEnergy[i] = energy(0, i);
+            minimumPointPosition[i][0][0] = 0;
+            minimumPointPosition[i][0][1] = i;
+        }
 
+        for (int i = 0; i < height(); i++) {
+            for (int j = 1; j < width() ; j++) {
+                minimumPointPosition[i][j] = minimumPixel(minimumPointPosition[i][j - 1][0], minimumPointPosition[i][j - 1][1]);
+                multiMinEnergy[i] += energy(minimumPointPosition[i][j][0], minimumPointPosition[i][j][1]);
+            }
+        }
+
+        double min = Double.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < height(); i++) {
+            if (min > multiMinEnergy[i]) {
+                min = multiMinEnergy[i];
+                index = i;
+            }
+        }
+
+        int[] minimumWay = new int[width()];
+        for (int i = 0; i < width(); i++) {
+            minimumWay[i] = minimumPointPosition[index][i][1];
+        }
+
+        return minimumWay;
     }            // sequence of indices for horizontal seam
 
 
 
     public int[] findVerticalSeam() {
-        double[] minCost = new double[width()];
-        ArrayList<Integer>[] paths = new ArrayList[width()];
+        isVertical = true;
+        int[][][] minimumPointPosition = new int[width()][height()][2];
+        double[] multiMinEnergy = new double[width()];
         for (int i = 0; i < width(); i++) {
-            minCost[i] = energy(0, i);
+            multiMinEnergy[i] = energy(i, 0);
+            minimumPointPosition[i][0][0] = i;
+            minimumPointPosition[i][0][1] = 0;
         }
+
         for (int i = 0; i < width(); i++) {
-            int[] initPoint = new int[]{0, i};
-            while (initPoint[0] < height() - 1) {
-                double minEnergy = Double.MAX_VALUE;
-                Integer[] minPoint = new Integer[2];
-                for (Integer[] point : nextMinimumPoints(initPoint[0], initPoint[1])) {
-                    double energy = energy(point[0], point[1]);
-                    if (energy < minEnergy) {
-                        minEnergy = energy;
-                        minPoint[0] = point[0];
-                        minPoint[1] = point[1];
-                    }
-                }
-                paths[i].add(minPoint[1]);
-                initPoint[0] = minPoint[0];
-                initPoint[1] = minPoint[1];
-                minCost[i] += minEnergy;
+            for (int j = 1; j < height() ; j++) {
+                minimumPointPosition[i][j] = minimumPixel(minimumPointPosition[i][j - 1][0], minimumPointPosition[i][j - 1][1]);
+                multiMinEnergy[i] += energy(minimumPointPosition[i][j][0], minimumPointPosition[i][j][1]);
             }
         }
-        int[] leastEnergyWay = new int[height()];
-        leastEnergyWay[0] = minIndex(minCost);
-        for (int i = 1; i < height(); i++) {
-            leastEnergyWay[i] = paths[leastEnergyWay[0]].get(i);
-        }
-        return leastEnergyWay;
-    }              // sequence of indices for vertical seam
 
-    private int minIndex(double[] minCost) {
         double min = Double.MAX_VALUE;
         int index = 0;
-        for (int i = 0; i < minCost.length; i++) {
-            if (minCost[i] < min) {
-                min = minCost[i];
+        for (int i = 0; i < width(); i++) {
+            if (min > multiMinEnergy[i]) {
+                min = multiMinEnergy[i];
                 index = i;
             }
         }
-        return index;
+
+        int[] minimumWay = new int[height()];
+        for (int i = 0; i < height(); i++) {
+            minimumWay[i] = minimumPointPosition[index][i][0];
+        }
+
+        return minimumWay;
+    }              // sequence of indices for vertical seam
+
+    private int[] minimumPixel(int initX, int initY) {
+        int[] minimumPixelPosition = new int[2];
+        if (isVertical) {
+            double min = Double.MAX_VALUE;
+            for (Integer[] point : nextMinimumPointsVertical(initX, initY)) {
+                double e = energy(point[0], point[1]);
+                if (e < min) {
+                    min = e;
+                    minimumPixelPosition[0] = point[0];
+                    minimumPixelPosition[1] = point[1];
+                }
+            }
+        } else {
+            double min = Double.MAX_VALUE;
+            for (Integer[] point : nextMinimumPointsHorizontal(initX, initY)) {
+                double e = energy(point[0], point[1]);
+                if (e < min) {
+                    min = e;
+                    minimumPixelPosition[0] = point[0];
+                    minimumPixelPosition[1] = point[1];
+                }
+            }
+        }
+        return minimumPixelPosition;
     }
 
-    private Iterable<Integer[]> nextMinimumPoints(int x, int y) {
+    private Iterable<Integer[]> nextMinimumPointsVertical(int x, int y) {
         List<Integer[]> points = new ArrayList<>();
-        if (y == 0) {
-            points.add(new Integer[]{x + 1, y});
-            points.add(new Integer[]{x + 1, y + 1});
-        } else if (y == width() - 1) {
-            points.add(new Integer[]{x + 1, y});
-            points.add(new Integer[]{x + 1, y - 1});
-        } else {
-            points.add(new Integer[]{x + 1, y - 1});
-            points.add(new Integer[]{x + 1, y});
-            points.add(new Integer[]{x + 1, y + 1});
+        if (y < height() - 1){
+            if (x == 0) {
+                points.add(new Integer[]{x + 1, y + 1});
+                points.add(new Integer[]{x, y + 1});
+            } else if (x == width() - 1) {
+                points.add(new Integer[]{x - 1, y + 1});
+                points.add(new Integer[]{x, y + 1});
+            } else {
+                points.add(new Integer[]{x - 1, y + 1});
+                points.add(new Integer[]{x, y + 1});
+                points.add(new Integer[]{x + 1, y + 1});
+            }
+        }
+        return points;
+    }
+
+    private Iterable<Integer[]> nextMinimumPointsHorizontal(int x, int y) {
+        List<Integer[]> points = new ArrayList<>();
+        if (x < width() - 1){
+            if (y == 0) {
+                points.add(new Integer[]{x + 1, y});
+                points.add(new Integer[]{x + 1, y + 1});
+            } else if (y == height() - 1) {
+                points.add(new Integer[]{x + 1, y});
+                points.add(new Integer[]{x + 1, y - 1});
+            } else {
+                points.add(new Integer[]{x + 1, y - 1});
+                points.add(new Integer[]{x + 1, y});
+                points.add(new Integer[]{x + 1, y + 1});
+            }
         }
         return points;
     }
 
     public void removeHorizontalSeam(int[] seam) {
-
+        if (seam.length != width() || !isValidSeam(seam)) {
+            throw new IllegalArgumentException();
+        }
+        SeamRemover.removeHorizontalSeam(pic, seam);
     }   // remove horizontal seam from picture
 
     public void removeVerticalSeam(int[] seam) {
-
+        if (seam.length != height() || !isValidSeam(seam)) {
+            throw new IllegalArgumentException();
+        }
+        SeamRemover.removeHorizontalSeam(pic, seam);
     }     // remove vertical seam from picture
+
+    private boolean isValidSeam(int[] seam) {
+        for (int i = 0, j = 1; j < seam.length; i++, j++) {
+            if (Math.abs(seam[i] - seam[j]) > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
